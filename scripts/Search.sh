401bccc4beeec28f391f89686a69605be699c79d
aisha almannai <aa2002255@qu.edu.qa>
2024-10-29 23:30:45 +0300

    send email

--- scripts/Search.sh
+++ scripts/Search.sh
@@ -4,3 +4,9 @@ date "+%d/%m/%Y" >> bigfile.txt
 find / -type f -size +1M &>> bigfile.txt
 cat bigfile.txt | wc -l >> bigfile.txt
 
+if [ -s bigfile.txt ]; then
+echo " contents of the bigfile are attached with this mail" | mail -s "bigfile content " -a $(cat bigfile.txt) QUID@qu.edu.qa 
+
+fi 
+
+
