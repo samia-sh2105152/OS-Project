3b7a9a004ab54ef8692cc3ef5e1d871b404f2c66
aisha almannai <aa2002255@qu.edu.qa>
2024-10-29 23:15:44 +0300

    save output into a file

--- scripts/Search.sh
+++ scripts/Search.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-echo "Files large than 1M "
-find / -type f -size +1M
+echo "Files large than 1M "> bigfile.txt
+date "+%d/%m/%Y" >> bigfile.txt
+find / -type f -size +1M &>> bigfile.txt
+cat bigfile.txt | wc -l >> bigfile.txt
+
