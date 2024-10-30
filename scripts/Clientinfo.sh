506ead42269a5adb86fee0dcb757c1d9bfeb60c1
aisha almannai <aa2002255@qu.edu.qa>
2024-10-30 19:27:47 +0300

    update clientinfo

--- scripts/Clientinfo.sh
+++ scripts/Clientinfo.sh
@@ -4,5 +4,8 @@ pstree
 # check the colum num 8 for letter Z zombie or letter X dead
 ps aux | awk '{ if ($8=="Z" || $8=="X" ) print $0}' 
 
-
-
+#CPU usage related to processes. 
+#top -i 
+#mpstat -P ALL
+#Memory usage of running processes. 
+ps aux | sort -rnk 4 | head -5
