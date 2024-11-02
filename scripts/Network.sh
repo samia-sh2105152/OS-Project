730fe2c8c93681f933a4e7eeb8d2f2633a885437
samia-sh2105152 <157452749+samia-sh2105152@users.noreply.github.com>
2024-11-02 22:04:51 +0300

    done

--- scripts/Network.sh
+++ scripts/Network.sh
@@ -6,7 +6,7 @@ output_network="network.log"
 
 server_ip="192.168.64.3"
 client1_ip="192.168.64.2"
-# client2_ip="192.168.64.2"
+client2_ip="192.168.64.2"
 
 echo "Server IP: $server_ip" >> "$output_network"
 echo "Client1 IP: $client1_ip" >> "$output_network"
@@ -31,19 +31,19 @@ do
         date "+%Y-%m-%d %H:%M:%S - Connectivity with client1 [$client1_ip] is ok." >> "$output_network"
     fi
     echo >> "$output_network"
-    # echo "Pinging from server (VM1) to client2 (VM3)..." >> "$output_network"
-    # ping -c 1 -W 2 "$client2_ip" >> "$output_network" 2>&1
-    # if [ $? -ne 0 ]; then
-    #     echo "Ping to client2[$client2_ip] failed. Running traceroute and diagnostic checks..." >> "$output_network"
-    #     ./traceroute.sh >> "$output_network" 2>&1ssh username@192.168.64.2
+    echo "Pinging from server (VM1) to client2 (VM3)..." >> "$output_network"
+    ping -c 1 -W 2 "$client2_ip" >> "$output_network" 2>&1
+    if [ $? -ne 0 ]; then
+         echo "Ping to client2[$client2_ip] failed. Running traceroute and diagnostic checks..." >> "$output_network"
+         ./traceroute.sh >> "$output_network" 2>&1ssh username@192.168.64.2
 
-    #     date "+%Y-%m-%d %H:%M:%S - Connectivity with client2 [$client2_ip] failed, running diagnostics." >> "$output_network"
-    # else
-    # 	echo >> "$output_network"
-    #   date "+%Y-%m-%d %H:%M:%S - Connectivity with client2 [$client2_ip] is ok." >> "$output_network"
-    #fi
+         date "+%Y-%m-%d %H:%M:%S - Connectivity with client2 [$client2_ip] failed, running diagnostics." >> "$output_network"
+    else
+	echo >> "$output_network"
+       date "+%Y-%m-%d %H:%M:%S - Connectivity with client2 [$client2_ip] is ok." >> "$output_network"
+    fi
 
-    # # echo >> "$output_network"
+    	echo >> "$output_network"
 
     echo "-------------------------------------------------" >> "$output_network"
     echo >> "$output_network"
@@ -74,19 +74,19 @@ do
     fi
     echo >> "$output_network"
 
-    # echo "Pinging from client2 (VM3) to server (VM1)..." >> "$output_network"
+    echo "Pinging from client2 (VM3) to server (VM1)..." >> "$output_network"
 
-    # ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no samia@$client1_ip "ping -c 1 -W 2 $server_ip" >> "$output_network" 2>&1
-    # ssh_exit_status=$?
+    ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no samia@$client1_ip "ping -c 1 -W 2 $server_ip" >> "$output_network" 2>&1
+    ssh_exit_status=$?
 
-    # if [ $ssh_exit_status -ne 0 ]; then
-    #     echo "Ping to server [$server_ip] failed. Running traceroute and diagnostic checks..." >> "$output_network"
-    #     ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no samia@$client1_ip "./traceroute.sh" >> "$output_network" 2>&1
-    #     date "+%Y-%m-%d %H:%M:%S - Connectivity with server [$server_ip] failed, running diagnostics." >> "$output_network"
-    # else
-    #     echo >> "$output_network"
-    #     date "+%Y-%m-%d %H:%M:%S - Connectivity with server [$server_ip] is ok." >> "$output_network"
-    # fi
+    if [ $ssh_exit_status -ne 0 ]; then
+        echo "Ping to server [$server_ip] failed. Running traceroute and diagnostic checks..." >> "$output_network"
+        ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no samia@$client1_ip "./traceroute.sh" >> "$output_network" 2>&1
+        date "+%Y-%m-%d %H:%M:%S - Connectivity with server [$server_ip] failed, running diagnostics." >> "$output_network"
+    else
+        echo >> "$output_network"
+        date "+%Y-%m-%d %H:%M:%S - Connectivity with server [$server_ip] is ok." >> "$output_network"
+    fi
  
     echo >> "$output_network"
     echo "-------------------------------------------------" >> "$output_network"
