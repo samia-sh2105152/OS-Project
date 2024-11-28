4c9afab56a5b339b82e3f7bf0ad8a612c43d34a9
ammnna <89984308+ammnna@users.noreply.github.com>
2024-11-28 22:03:19 +0300

    created client1 class but not tested

---
+++ Phase2/Clients/src/client1/Client1.java
@@ -0,0 +1,131 @@
+package client1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.concurrent.TimeUnit;
+
+public class Client1 {
+
+	public static void main(String[] args) throws IOException {
+		//runSerachScript();
+		//runClient();
+		runCheckScript();
+	}
+	public static void runClient() throws IOException {
+		
+		Socket client = null;
+		BufferedReader from_server = null;
+		PrintWriter to_server = null;
+
+		String serverInput, userInput;
+		//runSerachScript();
+
+		try {
+
+			client = new Socket("localhost", 1300);
+			from_server = new BufferedReader(new InputStreamReader(client.getInputStream()));
+			to_server = new PrintWriter(client.getOutputStream());
+
+			System.out.println("Connected with server " + client.getInetAddress() + ":" + client.getPort());
+
+			runLoginScript();
+			runCheckScript();
+			
+
+			while (true) {
+				to_server.println("Systeminfo");
+				to_server.flush();
+				serverInput = from_server.readLine();
+				
+				// if the server receives done just break from this loop
+				while (!(serverInput.equals("done") || serverInput == null)){
+					System.out.println(serverInput);
+					serverInput = from_server.readLine();
+					
+				}
+				//wating for 5 min
+				TimeUnit.MINUTES.sleep(5);
+				
+				
+				 
+				
+
+			} // end of the loop
+
+		} catch (IOException ioe) {
+			System.out.println("Error" + ioe);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				if (from_server != null)
+					from_server.close();
+				if (client != null)
+					client.close();
+				if (to_server != null)
+					to_server.close();
+			} catch (IOException ioee) {
+				System.err.println(ioee);
+			}
+		}
+
+	}
+
+	private static void runLoginScript() {
+		try {
+			Process pro = Runtime.getRuntime().exec("scripts/login.sh" );
+			BufferedReader search_output = new BufferedReader(new InputStreamReader(pro.getInputStream()));
+			String line = search_output.readLine();
+
+			while (line != null) {
+				System.out.println(line);
+				line = search_output.readLine();
+			}
+			pro.waitFor(1, TimeUnit.MINUTES);
+//			//Process bigFilePro = Runtime.getRuntime().exec("cat bigfile.txt " );
+//			BufferedReader bigFile_output = new BufferedReader(new InputStreamReader(bigFilePro.getInputStream()));
+//			String line2 = bigFile_output.readLine();
+//
+//			while (line2 != null) {
+//				System.out.println(line2);
+//				line2 = bigFile_output.readLine();
+//			}
+			
+			
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void runCheckScript() {
+		try {
+			ProcessBuilder pb = new ProcessBuilder("scripts/Check.sh");
+			pb.redirectErrorStream(true);
+			
+			//Process pro = Runtime.getRuntime().exec("scripts/Clientinfo.sh");
+			Process pro = pb.start();
+			BufferedReader search_output = new BufferedReader(new InputStreamReader(pro.getInputStream()));
+			String line = search_output.readLine();
+
+			while (line != null) {
+				System.out.println(line);
+				line = search_output.readLine();
+			}
+			pro.waitFor(10, TimeUnit.SECONDS);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	
+	}
+
+}
